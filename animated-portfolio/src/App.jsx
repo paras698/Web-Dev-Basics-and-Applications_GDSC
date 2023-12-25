a1c6724b70c4aa86eb866c91bb226be4bf3e7f2e
paras698 <118524567+paras698@users.noreply.github.com>
2023-12-25 22:45:56 +0530

    Add files via upload

---
+++ animated-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import "./app.scss"
+import Navbar from "./components/navbar/navbar";
+
+const App = () => {
+  return <div>
+    <section id="Homepage">
+      <Navbar/>
+      <Hero/>
+    </section>
+    <section id="Services">Parallax</section>
+    <section>Services</section>
+    <section id="Portfolio">Parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Contact">Contact</section>
+    {/* <Test/> */}
+  </div>;
+};
+
+export default App;
